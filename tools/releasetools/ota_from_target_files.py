3f9fc3b4191f2abdfe66b2d7aef02a8b5ace3df7
AndroidRul3z <ferrettidario@gmail.com>
2013-11-28 12:16:04 +0100

    [1/2] Backup tool support
    Change-Id: Iff58c2227ed9379f047c34b8eb6f7d8bec4c9248
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: joshuous <joshuous@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -198,6 +198,7 @@ OPTIONS.payload_signer_args = []
 OPTIONS.extracted_input = None
 OPTIONS.key_passwords = []
 OPTIONS.override_device = 'auto'
+OPTIONS.backuptool = True
 
 METADATA_NAME = 'META-INF/com/android/metadata'
 UNZIP_PATTERN = ['IMAGES/*', 'META/*']
@@ -363,6 +364,14 @@ def GetImage(which, tmpdir, where="IMAGES"):
       raise Exception('GetImage %s failed'%(img))
     return sparse_img.SparseImage(path, mappath, clobbered_blocks)
 
+def CopyInstallTools(output_zip):
+  oldcwd = os.getcwd()
+  os.chdir(os.getenv('OUT'))
+  for root, subdirs, files in os.walk("install"):
+    for f in files:
+      p = os.path.join(root, f)
+      output_zip.write(p, p)
+  os.chdir(oldcwd)
 
 def AddCompatibilityArchive(target_zip, output_zip, system_included=True,
                             vendor_included=True):
@@ -528,6 +537,11 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.SetPermissionsRecursive("/tmp/install", 0, 0, 0755, 0644, None, None)
   script.SetPermissionsRecursive("/tmp/install/bin", 0, 0, 0755, 0755, None, None)
 
+  if OPTIONS.backuptool:
+    script.Mount("/system")
+    script.RunBackup("backup")
+    script.Unmount("/system")
+
   system_progress = 0.75
 
   if OPTIONS.wipe_user_data:
@@ -581,8 +595,16 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     common.CheckSize(boot_img.data, "boot.img", OPTIONS.info_dict)
     common.ZipWriteStr(output_zip, "boot.img", boot_img.data)
 
-    script.ShowProgress(0.05, 5)
-    script.WriteRawImage("/boot", "boot.img")
+  if OPTIONS.backuptool:
+    script.ShowProgress(0.02, 10)
+    if OPTIONS.block_based:
+      script.Mount("/system")
+    script.RunBackup("restore")
+    if OPTIONS.block_based:
+      script.Unmount("/system")
+
+  script.ShowProgress(0.05, 5)
+  script.WriteRawImage("/boot", "boot.img")
 
   script.ShowProgress(0.2, 10)
   device_specific.FullOTA_InstallEnd()
