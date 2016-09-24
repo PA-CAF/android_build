163bce7adf6c9d7ed40f2c575f97e4b87491b57f
Alex Naidis <alex.naidis@linux.com>
2016-09-24 12:59:06 +0200

    build: enhance and extend support for SDCLANG
    * This commit enhances and extends the build system support for SDCLANG.
      With this commit SDCLANG is production ready.
    
    * SDCLANG can be enabled globally by setting SDCLANG to true.
      It can be enabled or disabled by setting LOCAL_SDCLANG accordingly.
    
    * Some security has been added to secure the usage of SDCLANG. Since
      SDCLANG is not supported on non-linux hosts, there is a big warning
      every time SDClANG is requested but cannot be used. Additionally,
      there is a flag SDCLANG_FORCED added. If this flag is enabled,
      the mentioned warning, transforms into an error. This flag can be used
      to ensure SDCLANG usage on production build if requested.
    
    * The SDCLANG specific flags were changed. "-mno-ae" which was later
      changed to SDLLVM_AE_FLAG was removed since this flag is designed for
      internal usage at Qualcomm only. The usage of "-Wno-vectorizer-no-neon"
      was streamlined to be used with the build system. The hacks were removed.
      Flags which are forced to SDCLANG from the build system appear at
      SDCLANG_PRECONFIGURED_FLAGS. Flags which can be added at compile time
      can be defined as SDCLANG_COMMON_FLAGS. These get appended to cflags,
      cppflags and conlyflags and can still be overriden by the target module.
    
    * We are storing the default settings for SDCLANG in
      device/qcom/common/sdclang/sdclang.mk. Check there for more information.
    
    * Also LOCAL_SDCLANG_EXTRA_FLAGS_32 and SDCLANG_FLAG_DEFS were removed
      since these are not defined anywhere.
    
    Change-Id: I61aad39b052405d4514874cb567d718fbe81a56b
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- core/binary.mk
+++ core/binary.mk
@@ -268,6 +268,18 @@ ifeq ($(SDCLANG),true)
     endif
 endif
 
+ifeq ($(my_sdclang),true)
+ifneq ($(HOST_OS),linux)
+$(warning ****************************************************************)
+$(warning * SDCLANG is not supported on non-linux hosts. Disabling...)
+$(warning ****************************************************************)
+my_sdclang := false
+ifeq ($(SDCLANG_FORCED),true)
+$(error $(LOCAL_PATH): SDCLANG_FORCED was triggered! You are not allowed to build without SDCLANG while it is enabled... Dying...)
+endif
+endif
+endif
+
 # arch-specific static libraries go first so that generic ones can depend on them
 my_static_libraries := $(LOCAL_STATIC_LIBRARIES_$($(my_prefix)$(LOCAL_2ND_ARCH_VAR_PREFIX)ARCH)) $(LOCAL_STATIC_LIBRARIES_$(my_32_64_bit_suffix)) $(my_static_libraries)
 my_whole_static_libraries := $(LOCAL_WHOLE_STATIC_LIBRARIES_$($(my_prefix)$(LOCAL_2ND_ARCH_VAR_PREFIX)ARCH)) $(LOCAL_WHOLE_STATIC_LIBRARIES_$(my_32_64_bit_suffix)) $(my_whole_static_libraries)
@@ -368,12 +380,6 @@ my_target_global_ldflags := $($(LOCAL_2ND_ARCH_VAR_PREFIX)CLANG_TARGET_GLOBAL_LD
     ifeq ($(my_sdclang),true)
         SDCLANG_PRECONFIGURED_FLAGS := -Wno-vectorizer-no-neon
 
-        ifeq ($(LOCAL_SDCLANG_LTO), true)
-        ifneq ($(LOCAL_MODULE_CLASS), STATIC_LIBRARIES)
-            SDCLANG_PRECONFIGURED_FLAGS += -flto
-            my_target_global_ldflags += -fuse-ld=qcld -flto
-        endif
-        endif
         my_target_global_cflags += $(SDCLANG_COMMON_FLAGS) $(SDCLANG_PRECONFIGURED_FLAGS)
         my_target_global_conlyflags += $(SDCLANG_COMMON_FLAGS) $(SDCLANG_PRECONFIGURED_FLAGS)
         my_target_global_cppflags += $(SDCLANG_COMMON_FLAGS) $(SDCLANG_PRECONFIGURED_FLAGS)
